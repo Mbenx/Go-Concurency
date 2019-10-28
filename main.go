d070cd0bca6708cb351a533bb8925ba5bd7172b1
Mbenx <herlambang.srihartono@gmail.com>
2019-10-28 10:43:08 +0700

    Part-4 Waitgroup

--- main.go
+++ main.go
@@ -2,47 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-// channel
-var itemsChannel = make(chan string)
-var cleansItemsChannel = make(chan string)
-var savedItemsChannel = make(chan string)
-
 func main() {
-	items := [7]string{"batu", "harta", "kerang", "harta", "batu", "harta", "kerang"}
-	// menyelam mencari harta karun
-	// terdiri dari banyak barang
-	// 1. Penyelam, 2. Pembersih 3. Penyimpan
-	// channel
+	var wg sync.WaitGroup
 
-	go penyelam(items)
-	go pembersih()
-	go penyimpan()
+	wg.Add(2)
+	go printText("Salam", &wg)
+	go printText("hallo", &wg)
 
-	time.Sleep(500 * time.Millisecond)
+	wg.Wait()
 }
 
-func penyelam(items [7]string) {
-	for _, item := range items {
-		if item == "harta" {
-			fmt.Println("Berhasil Mendapatkan" + item)
-			itemsChannel <- item
-		}
+func printText(text string, wg *sync.WaitGroup) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(text)
 	}
-}
-
-func pembersih() {
-	for rawItem := range itemsChannel {
-		fmt.Println("Berhasil Membersihkan" + rawItem)
-		cleansItemsChannel <- rawItem
-	}
-}
-
-func penyimpan() {
-	for rawItem := range cleansItemsChannel {
-		fmt.Println("Berhasil Menyimpan" + rawItem)
-		savedItemsChannel <- rawItem
-	}
+	wg.Done()
 }
